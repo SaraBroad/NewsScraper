001d792bab110799c5106adcd35005df027ff6d0
Sara Broad <saraebroad@gmail.com>
2018-06-09 21:37:30 -0400

    articles model

--- models/articles.js
+++ models/articles.js
@@ -1,24 +1,28 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
-var ArticleSchema = new Schema ({
-//title, summary, url
-title: {
-    type: String,
-    required: true
-},
+var ArticleSchema = new Schema({
+    //title, summary, url
+    title: {
+        type: String,
+        required: true
+    },
 
-summary: {
-    type: String,
-    required: true
-},
+    summary: {
+        type: String,
+        required: true
+    },
 
-url: {
-    type: String,
-    required: true
-}
+    url: {
+        type: String,
+        required: true
+    },
+
+    note: {
+        type: Schema.Type.ObjectId,
+        ref: "Note"
+    }
 });
-
 
 var Article = mongoose.model("Article", ArticleSchema);
 module.exports = Article;
--- models/notes.js
+++ models/notes.js
@@ -1,8 +1,14 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
-var NoteSchema = new Schema ({
-
+var NoteSchema = new Schema({
+    text: {
+        type: String,
+        required: true
+    },
+    lastUpdated: {
+        type: Date
+    }
 });
 
 var Note = mongoose.model("Note", NoteSchema);
