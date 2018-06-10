5e443f388fc1c261a82cdf6a976a0b813ec2dc4a
Sara Broad <saraebroad@gmail.com>
2018-06-09 22:21:52 -0400

    public

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
+var exphbs = require("express-handlebars");
 var mongoose = require("mongoose");
 var cheerio = require("cheerio");
 var request = require("request");
@@ -12,17 +13,17 @@ var PORT = process.env.PORT || 8080;
 var app = express();
 
 app.use(logger("dev"));
-// app.use(express.static("public"));
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-var exphbs = require("express-handlebars");
+
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-mongoose.connect("mongodb://localhost/NewsScraper");
+// mongoose.connect("mongodb://localhost/NewsScraper");
 
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
