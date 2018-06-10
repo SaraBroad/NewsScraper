2559df576ce027ee0775a136c4b4b65cc45e8338
Sara Broad <saraebroad@gmail.com>
2018-06-09 20:23:56 -0400

    models

--- models/articles.js
+++ models/articles.js
@@ -1,7 +1,12 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
-var Article = new Schema ({
+var ArticleSchema = new Schema ({
 
-    
+
 });
+
+
+var Article = mongoose.model("Article", ArticleSchema);
+module.exports = Article;
+
