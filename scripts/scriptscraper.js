bb0036e472cc76e82e31c70b49819832a8c6b7a5
Sara Broad <saraebroad@gmail.com>
2018-06-12 16:50:26 -0400

    get articles

--- scripts/scriptscraper.js
+++ scripts/scriptscraper.js
@@ -7,19 +7,26 @@ var axios = require("axios");
 //when array is complete (outside of loop) return articles
 //when hit scrape button, the route will go to the controller which uses this file at which point this file will 
 //this function should return an array of articles
-module.exports = function() {
-    app.get("/scrape", function (req, res){
+module.exports = function () {
+    app.get("/scrape", function (req, res) {
         var $ = cheerio.load(response.data);
-        axios.get("https://www.bbc.com").then(function(response){
-            
+        axios.get("https://www.bbc.com").then(function (response) {
+
             var result = {};
 
-            $(".media__content").each(function(i, element){
-            var title = $(element).children('h3').children("a").text();
-            var link = $(element).children("a").attr("href");
-            var summary = $(element).children("p").text();
-            
-            }); 
+            $(".media__content").each(function (i, element) {
+                var title = $(element).children('h3').children("a").text();
+                var link = $(element).children("a").attr("href");
+                var summary = $(element).children("p").text();
+
+                db.Article.create(result)
+                    .then(function (dbArticle) {
+                        console.log(dbArticle)
+                    }).catch(function (err) {
+                        return res.json(err);
+                    })
+            })
+            res.send("Scrape complete");
         })
-    });
+    })
 };
